757133877aa015245eac93fd52aa14dddee8f4b7
Yashwant <yashswami284@gmail.com>
2022-10-03 06:20:48 +0530

    Video 19

--- src/App.js
+++ src/App.js
@@ -31,12 +31,7 @@ function App() {
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark mode has been enable", "success")
       document.title = 'TextUtils - Dark Mode';
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing Mode';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
+      
     }
     else{
       setMode('light');
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,11 +19,6 @@ export default function Navbar(props) {
                             <Link className="nav-link" to="/about">{props.aboutText}</Link>
                         </li>
                     </ul>
-                    {/* <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button cla
-                        ssName="btn btn-primary" type="submit">Search</button>
-                    </form> */}
                     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
