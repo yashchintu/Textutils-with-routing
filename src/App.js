7f8496cc74c116a700616512d219914d7c81911f
Yashwant <yashswami284@gmail.com>
2022-10-03 06:13:30 +0530

    Video 20 completed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
+  const [mode, setMode] = useState('light'); //Wheather Dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+  
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+}
+  
+  const toggleMode = (cls)=>{
+    removeBodyClasses()
+    document.body.classList.add('bg-' + cls)
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enable", "success")
+      // document.title = 'TextUtils - Dark Mode';      
+    }
+
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enable", "success")
+      // document.title = 'TextUtils - Light Mode';
+    }
+  }
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+    <BrowserRouter>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils " mode={mode} toggleMode={toggleMode}/> {/* togglemode ek funtion h  ya props h*/}
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+            <Route path="/about" element={<About  mode={mode} />}>
+            </Route>
+            <Route path="/" element={<TextForm heading="Try Textutils - Word Counter, Character Counter, Remove extra spaces"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+    </Routes>
+    </div> 
+    </BrowserRouter>
     </>
   );
 }
