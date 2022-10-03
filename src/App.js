58253a8adad96e6ef686164613477bb9fd126d4a
Yashwant <yashswami284@gmail.com>
2022-10-03 07:26:13 +0530

    Video20

--- src/App.js
+++ src/App.js
@@ -24,14 +24,22 @@ function App() {
     }, 1500);
   }
   
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+}
   
-  const toggleMode = ()=>{
+  const toggleMode = (cls)=>{
+    removeBodyClasses()
+    document.body.classList.add('bg-' + cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark mode has been enable", "success")
       //document.title = 'TextUtils - Dark Mode';
-      
     }
     else{
       setMode('light');
@@ -51,7 +59,7 @@ function App() {
     <Routes>
             <Route path="/about" element={<About />}>
             </Route>
-            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            <Route path="/" element={<TextForm heading="Try Textutils - Word Counter, Character Counter, Remove extra spaces"  mode={mode} showAlert={showAlert} />}>
             </Route>
     </Routes>
     </div> 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,17 @@ export default function Navbar(props) {
                             <Link className="nav-link" to="/about">{props.aboutText}</Link>
                         </li>
                     </ul>
-                    
+
+                    <div className="d-flex">
+                        <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height:'30px', width:'30px', cursor:'pointer'}}></div>
+                        <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height:'30px', width:'30px', cursor:'pointer'}}></div>
+                        <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height:'30px', width:'30px', cursor:'pointer'}}></div>
+                        <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height:'30px', width:'30px', cursor: 'pointer'}}></div>
+                    </div>
+
                     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                        <input className="form-check-input" onClick={()=>{props.toggleMode(null)}} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
                     </div>
                 </div>
             </div>
